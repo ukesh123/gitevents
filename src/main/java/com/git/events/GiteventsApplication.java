6495514f542d9032697f7b5b039ad8b86acd395e
ukesh123 <ukesh123@localhost>
2020-07-10 19:44:36 +0530

    Initial Commit

---
+++ src/main/java/com/git/events/GiteventsApplication.java
@@ -0,0 +1,13 @@
+package com.git.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiteventsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/git/events/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.git.events;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GiteventsApplication.class);
+	}
+
+}
